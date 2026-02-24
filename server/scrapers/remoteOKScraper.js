2572d491488f7ba3f99aba154c88ab924fd06530
Kajal Tiwari <kajaltwari7408@gmail.com>
2026-02-24 22:14:41 +0530

    update

--- server/scrapers/remoteOKScraper.js
+++ server/scrapers/remoteOKScraper.js
@@ -5,14 +5,9 @@ const scrapeRemoteOK = async () => {
   console.log("🚀 Fetching RemoteOK API...");
 
   try {
-    const response = await axios.get('https://remoteok.io/api', {
-      headers: {
+const response = await axios.get('https://remoteok.com/remote-dev-jobs.json', {      headers: {
         'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
-          '(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'Accept': 'application/json',
-        'Origin': 'https://remoteok.io',
-        'Referer': 'https://remoteok.io/'
+          'Mozilla/5.0'
       }
     });
 
